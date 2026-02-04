8ee336f9b99c6e11d013e9b715aa706392fbc8f0
cobrazohaib <zohaibaamer2001@gmail.com>
2026-02-04 17:53:36 +0500

    Refactor client deletion functionality to improve reliability and user experience

---
+++ backend/alembic/versions/d7ffe8426a29_add_is_deleted.py
@@ -0,0 +1,27 @@
+"""add is_deleted
+
+Revision ID: d7ffe8426a29
+Revises: add_is_deleted_to_users
+Create Date: 2026-02-04 16:50:25.547373
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd7ffe8426a29'
+down_revision = 'add_is_deleted_to_users'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
+
+
